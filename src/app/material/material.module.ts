708e8bb94d9b06ee0a8781f29ec8313a5293afa8
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2023-01-12 14:21:54 -0300

    💄 feat: creating the header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
+  exports: [
     MatDialogModule,
     MatButtonModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
