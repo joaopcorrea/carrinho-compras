c917b9b6702c3ca384d1f8047cf00663d9786377
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2023-01-16 18:27:03 -0300

    💄 feat: creating search component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,25 @@
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from './../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ProductFormComponent } from './product-form/product-form.component';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
